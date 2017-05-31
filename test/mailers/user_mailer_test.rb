eca4b21577f80df0350151dfe1e838b34aac6420
SangPham <anhsang222@gmail.com>
2017-05-31 11:11:41 +0700

    tam xong bai 11

---
+++ test/mailers/user_mailer_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class UserMailerTest < ActionMailer::TestCase
+  test "account_activation" do
+    mail = UserMailer.account_activation
+    assert_equal "Account activation", mail.subject
+    assert_equal ["to@example.org"], mail.to
+    assert_equal ["from@example.com"], mail.from
+    assert_match "Hi", mail.body.encoded
+  end
+
+  test "pasword_reset" do
+    mail = UserMailer.pasword_reset
+    assert_equal "Pasword reset", mail.subject
+    assert_equal ["to@example.org"], mail.to
+    assert_equal ["from@example.com"], mail.from
+    assert_match "Hi", mail.body.encoded
+  end
+
+end
